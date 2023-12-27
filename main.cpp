3b42b9ac51c27f9d207f7bae48baf2324dcb56ae
nickb <n.j.buhling@student.utwente.nl>
2023-12-27 22:08:33 +0100

    Revert "First commit (test)"
    This reverts commit 29078a842d26e1b0704bbf761acaeb833ab64101.
    
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -239,7 +239,7 @@ public:
         }
     }
 
-    // TODO: if more than x cards, print in two rows (TEST NEW BRANCH)
+    // TODO: if more than x cards, print in two rows
     void printHorizontal() {
         // Printing the top borders
         for (Card cardToPrint: cardsInHand) {
